12f470422d905fc06b6857af35efffa5de8cb666
LiceyMaxim <maks.semyonov.2004@gmail.com>
2024-01-11 23:16:20 +0300

    lab_4

--- static/js/page-load-stats.js
+++ static/js/page-load-stats.js
@@ -1,12 +1,14 @@
 (function() {
-  document.addEventListener('DOMContentLoaded', function() {
-    var loadTime = window.performance.timing.domContentLoadedEventEnd - window.performance.timing.navigationStart;
-    console.log('Page load time is ' + loadTime + ' milliseconds.');
-    var footer = document.querySelector('footer');
-    if (footer) {
-      var statsParagraph = document.createElement('p');
-      statsParagraph.textContent = 'Page loaded in ' + loadTime + ' milliseconds.';
-      footer.appendChild(statsParagraph);
-    }
-  });
+ let startTime = performance.now();
+ document.addEventListener('DOMContentLoaded', function() {
+   let endTime = performance.now();
+   let loadTime = endTime - startTime;
+   console.log('Page load time is ' + loadTime + ' milliseconds.');
+   var footer = document.querySelector('footer');
+   if (footer) {
+     var statsParagraph = document.createElement('p');
+     statsParagraph.textContent = 'Page loaded in ' + loadTime + ' milliseconds.';
+     footer.appendChild(statsParagraph);
+   }
+ });
 })();
